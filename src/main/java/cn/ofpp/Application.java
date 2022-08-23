aa38fc1d783aa379fa1ca7940ee980b6eff366a1
ONEFK <111872181+ONEFK@users.noreply.github.com>
2022-08-23 22:06:28 +0800

    Update Application.java

--- src/main/java/cn/ofpp/Application.java
+++ src/main/java/cn/ofpp/Application.java
@@ -29,7 +29,7 @@ public class Application {
 
         // new 一个 女友
         GirlFriend girlFriend = new GirlFriend("小阳宝宝",
-                "湖南省", "永州市", "2004-05-18", "2022-02-25", "oxeY16AFzjuhE_mOD6SgrmGJgtvw");
+                "湖南省", "永州市", "2003-05-18", "2022-02-25", "oxeY16AFzjuhE_mOD6SgrmGJgtvw");
         Wx.sendTemplateMessage(MessageFactory.resolveMessage(girlFriend));
 
         // new 一个 男友 也可单独针对一个friend设置模板ID 以达到不同人不同消息
